cd678e27197029a2035569326f87c39a09732bab
scrazym <76552079+scrazym@users.noreply.github.com>
2023-10-19 11:49:04 +0300

    Update const.ts api key

--- src/api/consts/const.ts
+++ src/api/consts/const.ts
@@ -1,2 +1,2 @@
 export const BASE_URL = "https://api.weatherapi.com/v1";
-export const WEATHERAPI_KEY = "bbf4524377fd488ebfd122939232308";
+export const WEATHERAPI_KEY = "ad7bf7ca6ad54c8caed84039231910";
